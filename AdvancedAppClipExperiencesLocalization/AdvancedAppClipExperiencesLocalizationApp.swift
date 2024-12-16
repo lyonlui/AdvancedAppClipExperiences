7d5d1c874184570f1e55c27d195f414a44db6669
lyonlui <lyonlui@gmail.com>
2024-12-16 11:05:09 +0800

    Initial Commit

---
+++ AdvancedAppClipExperiencesLocalization/AdvancedAppClipExperiencesLocalizationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdvancedAppClipExperiencesLocalizationApp.swift
+//  AdvancedAppClipExperiencesLocalization
+//
+//  Created by zhiwei liu on 2024/12/16.
+//
+
+import SwiftUI
+
+@main
+struct AdvancedAppClipExperiencesLocalizationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
